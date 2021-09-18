daacf1069689ee2d33d84c736faea98930a6dcd2
Kairamkondapranathi <89980698+Kairamkondapranathi@users.noreply.github.com>
2021-09-18 20:14:56 +0530

    Update TypeCasting.java

--- TypeCasting.java
+++ TypeCasting.java
@@ -7,8 +7,7 @@ public class TypeCasting {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter a number");
 		int i=scan.nextInt();
-		long a=i;
-		float f=a;
+		float f=i;
 		
 		System.out.println("Int value "+i); 
         System.out.println("Float value" +f); 
